46483b22e59e4fd46ac87fb73d13360a5497c06b
alexandre-jm <alexandre.rodrigues.silva@jeronimo-martins.com>
2025-02-25 17:35:20 +0000

    Update environment.prod.ts

--- firewall-mon-app/src/environments/environment.prod.ts
+++ firewall-mon-app/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   ApplicationInsightsConnectionString: "__APPLICATION_INSIGHTS_CONNECTION_STRING__",
   BuildDate: "__BUILDDATE__",
-  EventsQueueLength: 100000,
+  EventsQueueLength: 10000000,
 };
